cdad061c21ff4d0042a9286953f6d0cfd2d3533f
KoganeRoguen <144063450+KoganeRoguen@users.noreply.github.com>
2023-12-08 17:55:49 -0300

    Update Life Simulator.py

--- Mini Projeto/Life Simulator.py
+++ Mini Projeto/Life Simulator.py
@@ -1,12 +1,11 @@
-import subprocess,random,time,os
+import os
+from tkinter import Tk, Label, Button, Toplevel
 
 # Interfaces
-
 status = os.path.join(os.path.expanduser('~'), 'Documents', 'status.txt')
 local = os.path.join(os.path.expanduser('~'), 'Documents', 'local.txt')
 
 # Status do personagem
-
 food = 100
 water = 100
 energia = 100
@@ -18,20 +17,21 @@ urina = 0
 felicidade = 100
 
 # Geladeira
-
 comida = 10
 água = 10
 
 # Informar o seu nome
-
 name = input('\n> Informe o seu nome: ')
 
-with open(status, 'w') as file:
-    content = f'''____________________
+# Criar janela principal
+root = Tk()
+root.title("Status")
+root.geometry("400x300")
 
+# Labels com status do personagem
+status_label = Label(root, text=f'''
 Nome: {name}
 ____________________
-
 > Alimentação [{food}%]
 > Hidratação [{water}%]
 > Energia(sono) [{energia}%]
@@ -41,21 +41,65 @@ ____________________
 > Intestino [{intestino}%]
 > Bexiga [{urina}%]
 > Felicidade [{felicidade}%]
-'''
-    file.write(content)
-    subprocess.Popen(['notepad.exe', status])
+''')
+status_label.pack()
 
-with open(local, 'w') as file:
-    content = f'''____________________
-
-      = Casa =
+# Função para atualizar status
+def update_status():
+    status_label.config(text=f'''
+Nome: {name}
 ____________________
+> Alimentação [{food}%]
+> Hidratação [{water}%]
+> Energia(sono) [{energia}%]
+> Força {força}
+> Inteligência {inteligencia}
+> Higienização [{higiene}%]
+> Intestino [{intestino}%]
+> Bexiga [{urina}%]
+> Felicidade [{felicidade}%]
+''')
 
-> Abrir geladeira
-> Dormir
-> Usar o banheiro
-> Ler Livro escolar
-> Desenhar com lápis
-'''
-    file.write(content)
-    subprocess.Popen(['notepad.exe', local])
+# Janela para as opções do local
+def open_local_options():
+    local_window = Toplevel(root)
+    local_window.title("Casa")
+    local_window.geometry("400x300")
+    abrir_geladeira_button = Button(local_window, text='Abrir geladeira', command=open_fridge)
+    abrir_geladeira_button.pack()
+    dormir_button = Button(local_window, text='Dormir', command=go_to_sleep)
+    dormir_button.pack()
+    usar_banheiro_button = Button(local_window, text='Usar o banheiro', command=use_bathroom)
+    usar_banheiro_button.pack()
+    ler_livro_button = Button(local_window, text='Ler Livro escolar', command=read_book)
+    ler_livro_button.pack()
+    desenhar_button = Button(local_window, text='Desenhar com lápis', command=draw)
+    desenhar_button.pack()
+
+# Lógica para as ações de cada botão
+def open_fridge():
+    # Lógica para abrir a geladeira
+    update_status()
+
+def go_to_sleep():
+    # Lógica para dormir
+    update_status()
+
+def use_bathroom():
+    # Lógica para usar o banheiro
+    update_status()
+
+def read_book():
+    # Lógica para ler um livro
+    update_status()
+
+def draw():
+    # Lógica para desenhar
+    update_status()
+
+# Botão para abrir a janela com as opções do local
+open_local_button = Button(root, text="Local", command=open_local_options)
+open_local_button.pack()
+
+# Loop principal da interface
+root.mainloop()
